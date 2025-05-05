f3e21cf422b313af0c71ceced1aba2b1b8bf1e74
n10dz <dp7100@126.com>
2025-05-05 18:13:19 +0800

    鏇存柊婧愮爜: 2025/05/05 周一 18:13:19.04

--- start.bat
+++ start.bat
@@ -6,12 +6,12 @@ REM 进入public目录提交
 cd public
 git add .
 git commit -m "自动部署: %date% %time%"
-git push origin gh-pages
+git push -f origin gh-pages
 cd ..
 
 REM 提交源码（可选）
 git add .
 git commit -m "更新源码: %date% %time%"
-git push origin main
+git push -f origin main
 
 pause
